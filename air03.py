2a96520b13df6870f614a62e6d92b9f2a406cb9e
MickaelMasson <masson.mickael@me.com>
2024-11-22 16:44:40 +0100

    Chercher l'intrus

--- air03.py
+++ air03.py
@@ -1,35 +1,21 @@
-"""
-"""
+"""Chercher l’intrus"""
 import sys
 
 # Fonctions utilisées
+def get_single_arguments(arguments) :
+    single_arguments = []
+    for argument in arguments :
+        if argument in single_arguments :
+            single_arguments.remove(argument)
+        else :
+            single_arguments.append(argument)
+    return single_arguments
 
 # Partie 1 : Gestion d'erreur
 def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 3 :
+    if len(arguments) < 1 :
         print("Error, ")
         return False
-    return True
-    
-def is_digit(arguments) :
-    for argument in arguments :
-        if not argument.lstrip("-").isdigit() :
-            print("Error, ")
-            return False
-    return True
-
-def is_valid_number(arguments) :
-    number = int(arguments[0])
-    if number < 1 :
-        print(-1)
-        return False
-    return True
-
-def is_alpha(arguments):
-    for argument in arguments :
-        if not argument.isalpha() :
-            print("Vous devez saisir des arguments composés de lettres a-z / A-Z")
-            return False
     return True
 
 # Partie 2 : Parsing
@@ -38,11 +24,23 @@ def get_arguments() :
     return arguments
 
 # Partie 3 : Résolution
-def display() :
-    
+def display_single_argument() :
+    if not is_valid_number_of_arguments(get_arguments()) :
+        return
+    print(", ".join(get_single_arguments(get_arguments())))
 
 # Partie 4 : Affichage
-display()
+display_single_argument()
 """
+Créez un programme qui retourne la valeur d’une liste qui n’a pas de paire.
+
+Exemples d’utilisation :
+$> python exo.py 1 2 3 4 5 4 3 2 1
+5
+
+$> python exo.py bonjour monsieur bonjour
+monsieur
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 """
