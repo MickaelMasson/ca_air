be795e3e9a0f591d80d091232dc49155b3d49844
MickaelMasson <masson.mickael@me.com>
2024-11-22 16:34:54 +0100

    Concat

--- air02.py
+++ air02.py
@@ -1,35 +1,18 @@
-"""
-"""
+"""Concat"""
 import sys
 
 # Fonctions utilisées
+def get_new_string(arguments) :
+    separator = arguments[-1]
+    arguments = arguments[0:-1]
+    news_string = separator.join(arguments)
+    return news_string
 
 # Partie 1 : Gestion d'erreur
 def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 3 :
-        print("Error, ")
+    if len(arguments) < 2 :
+        print("Error, vous devez saisir 2 arguments")
         return False
-    return True
-    
-def is_digit(arguments) :
-    for argument in arguments :
-        if not argument.lstrip("-").isdigit() :
-            print("Error, ")
-            return False
-    return True
-
-def is_valid_number(arguments) :
-    number = int(arguments[0])
-    if number < 1 :
-        print(-1)
-        return False
-    return True
-
-def is_alpha(arguments):
-    for argument in arguments :
-        if not argument.isalpha() :
-            print("Vous devez saisir des arguments composés de lettres a-z / A-Z")
-            return False
     return True
 
 # Partie 2 : Parsing
@@ -38,11 +21,29 @@ def get_arguments() :
     return arguments
 
 # Partie 3 : Résolution
-def display() :
-    
+def display_news_string() :
+    if not is_valid_number_of_arguments(get_arguments()) :
+        return
+    print(get_new_string(get_arguments()))
 
 # Partie 4 : Affichage
-display()
+display_news_string()
 """
+Créez un programme qui transforme un tableau de chaînes de caractères 
+en une seule chaîne de caractères. Espacés d’un séparateur donné en dernier argument au programme.
+
+Utilisez une fonction de ce genre (selon votre langage) :
+ma_fonction(array_de_strings, separateur) {
+	# votre algorithme
+	return (string)
+}
+
+
+Exemples d’utilisation :
+$> python exo.py “je” “teste” “des” “trucs” “ “
+Je teste des trucs
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 """
