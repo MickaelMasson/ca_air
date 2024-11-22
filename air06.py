fa6c9484d54ad4b863efec41aaf5dac353262f89
MickaelMasson <masson.mickael@me.com>
2024-11-22 18:42:27 +0100

    Controle de pass sanitaie

--- air06.py
+++ air06.py
@@ -1,35 +1,22 @@
-"""
-"""
+"""Contrôle de pass sanitaire"""
 import sys
 
 # Fonctions utilisées
+def is_in_string(arguments):
+    base_arguments = arguments[0:-1]
+    ref_argument = str(arguments[-1]).lower()
+    news_names = []
+    for i in base_arguments :
+        #name = str(i).lower()
+        if not ref_argument in str(i).lower() :
+            news_names.append(i)
+    return news_names
 
 # Partie 1 : Gestion d'erreur
 def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 3 :
-        print("Error, ")
+    if len(arguments) < 2 :
+        print("Error, vous devez saisir au moins 2 arguments")
         return False
-    return True
-    
-def is_digit(arguments) :
-    for argument in arguments :
-        if not argument.lstrip("-").isdigit() :
-            print("Error, ")
-            return False
-    return True
-
-def is_valid_number(arguments) :
-    number = int(arguments[0])
-    if number < 1 :
-        print(-1)
-        return False
-    return True
-
-def is_alpha(arguments):
-    for argument in arguments :
-        if not argument.isalpha() :
-            print("Vous devez saisir des arguments composés de lettres a-z / A-Z")
-            return False
     return True
 
 # Partie 2 : Parsing
@@ -39,10 +26,31 @@ def get_arguments() :
 
 # Partie 3 : Résolution
 def display() :
-    
+    if not is_valid_number_of_arguments(get_arguments()) :
+        return
+    print(", ".join(is_in_string(get_arguments())))
 
 # Partie 4 : Affichage
 display()
 """
+Créez un programme qui supprime d’un tableau tous les éléments qui ne contiennent pas une autre chaîne de caractères.
+
+Utilisez une fonction de ce genre (selon votre langage) :
+ma_fonction(array_de_strings, string) {
+	# votre algorithme
+	return (nouvel_array_de_strings)
+}
+
+
+Exemples d’utilisation :
+$> python exo.py “Michel” “Albert” “Thérèse” “Benoit” “t”
+Michel
+
+$> python exo.py “Michel” “Albert” “Thérèse” “Benoit” “a”
+Michel, Thérèse, Benoit
+
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 """
