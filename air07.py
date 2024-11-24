c9170daad8869c2cc6981336a36886a13a598a58
MickaelMasson <masson.mickael@me.com>
2024-11-24 06:15:40 +0100

    Insertion dans un tableau trié

--- air07.py
+++ air07.py
@@ -1,34 +1,25 @@
-"""
-"""
+"""Insertion dans un tableau trié"""
 import sys
 
 # Fonctions utilisées
+def add_number_sort_list(arguments):
+    base_arguments = arguments[:-1]
+    new_number = arguments[-1]
+    base_arguments.append(new_number)
+    new_sort_list = sorted(base_arguments)
+    return new_sort_list
 
 # Partie 1 : Gestion d'erreur
 def is_valid_number_of_arguments(arguments) :
     if len(arguments) < 3 :
-        print("Error, ")
+        print("Error, vous devez saisir au moins 3 arguments")
         return False
     return True
     
 def is_digit(arguments) :
     for argument in arguments :
         if not argument.lstrip("-").isdigit() :
-            print("Error, ")
-            return False
-    return True
-
-def is_valid_number(arguments) :
-    number = int(arguments[0])
-    if number < 1 :
-        print(-1)
-        return False
-    return True
-
-def is_alpha(arguments):
-    for argument in arguments :
-        if not argument.isalpha() :
-            print("Vous devez saisir des arguments composés de lettres a-z / A-Z")
+            print("Error, tous vos arguments doivent être des entier")
             return False
     return True
 
@@ -39,10 +30,32 @@ def get_arguments() :
 
 # Partie 3 : Résolution
 def display() :
-    
+    if not is_valid_number_of_arguments(get_arguments()) :
+        return
+    if not is_digit(get_arguments()) :
+        return
+    print(" ".join(add_number_sort_list(get_arguments())))
 
 # Partie 4 : Affichage
 display()
 """
+Créez un programme qui ajoute à une liste d’entiers triée un nouvel entier tout en gardant la liste triée dans l’ordre croissant. Le dernier argument est l’élément à ajouter.
+
+Utilisez une fonction de ce genre (selon votre langage) :
+sorted_insert(array, new_element) {
+	# your algo
+	return (new_array)
+}
+
+
+Exemples d’utilisation :
+$> ruby exo.rb 1 3 4 2
+1 2 3 4
+
+$> ruby exo.rb 10 20 30 40 50 60 70 90 33
+10 20 30 33 40 50 60 70 90
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 """
