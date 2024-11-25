79e7704e7892bcfb3349cd5a6406e7339452d9d4
MickaelMasson <masson.mickael@me.com>
2024-11-25 11:54:47 +0100

    Rotation vers la gauche

--- air09.py
+++ air09.py
@@ -1,35 +1,19 @@
-"""
-"""
+"""Rotation vers la gauche"""
 import sys
 
 # Fonctions utilisées
+def shifts_to_the_left(arguments) :
+    first_argument = arguments[0]
+    arguments.remove(first_argument)
+    new_list = arguments
+    new_list.append(first_argument)
+    return new_list
 
 # Partie 1 : Gestion d'erreur
 def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 3 :
-        print("Error, ")
+    if len(arguments) < 2 :
+        print("Error, vous deve saisir au moins deux arguments")
         return False
-    return True
-    
-def is_digit(arguments) :
-    for argument in arguments :
-        if not argument.lstrip("-").isdigit() :
-            print("Error, ")
-            return False
-    return True
-
-def is_valid_number(arguments) :
-    number = int(arguments[0])
-    if number < 1 :
-        print(-1)
-        return False
-    return True
-
-def is_alpha(arguments):
-    for argument in arguments :
-        if not argument.isalpha() :
-            print("Vous devez saisir des arguments composés de lettres a-z / A-Z")
-            return False
     return True
 
 # Partie 2 : Parsing
@@ -38,11 +22,28 @@ def get_arguments() :
     return arguments
 
 # Partie 3 : Résolution
-def display() :
-    
+def display_new_list() :
+    if not is_valid_number_of_arguments(get_arguments()) :
+        return
+    print(", ".join(shifts_to_the_left(get_arguments())))
 
 # Partie 4 : Affichage
-display()
+display_new_list()
 """
+Créez un programme qui décale tous les éléments d’un tableau vers la gauche. Le premier élément devient le dernier à chaque rotation.
+
+Utilisez une fonction de ce genre (selon votre langage) :
+ma_rotation(array) {
+	# votre algorithme
+	return (new_array)
+}
+
+
+Exemples d’utilisation :
+$> python exo.py “Michel” “Albert” “Thérèse” “Benoit”
+Albert, Thérèse, Benoit, Michel
+
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 """
