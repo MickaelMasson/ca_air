9101a33a0ee25d4cc79370adcd89d1c3ace2e851
MickaelMasson <masson.mickael@me.com>
2024-11-27 06:02:55 +0100

    Afficher une pyramide

--- air11.py
+++ air11.py
@@ -1,34 +1,32 @@
-"""
-"""
+"""Afficher une pyramide"""
 import sys
 
 # Fonctions utilisées
+def get_pyramid(arguments) :
+    character = arguments[0]
+    number_of_floors = int(arguments[1])
+    for i in range(number_of_floors) :
+         print(" " * (number_of_floors - (i + 1)) + character * ((2 * i) + 1))
+    return
 
 # Partie 1 : Gestion d'erreur
 def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 3 :
-        print("Error, ")
+    if len(arguments) != 2 :
+        print("Error, vous devez saisir 2 arguments")
         return False
     return True
-    
-def is_digit(arguments) :
-    for argument in arguments :
-        if not argument.lstrip("-").isdigit() :
-            print("Error, ")
+
+def is_single_character(arguments):
+    argument = arguments[0]
+    if len(argument) != 1 :
+            print("Vous devez saisir un seul caractère en 1er arguments")
             return False
     return True
-
-def is_valid_number(arguments) :
-    number = int(arguments[0])
-    if number < 1 :
-        print(-1)
-        return False
-    return True
-
-def is_alpha(arguments):
-    for argument in arguments :
-        if not argument.isalpha() :
-            print("Vous devez saisir des arguments composés de lettres a-z / A-Z")
+    
+def is_digit_number_of_floors(arguments) :
+    argument = arguments[1]
+    if not argument.isdigit() :
+            print("Error, vous devez entrez un entier positif en 2eme argument")
             return False
     return True
 
@@ -38,11 +36,28 @@ def get_arguments() :
     return arguments
 
 # Partie 3 : Résolution
-def display() :
-    
+def display_pyramid() :
+    if not is_valid_number_of_arguments(get_arguments()) :
+        return
+    if not is_digit_number_of_floors(get_arguments()) :
+        return
+    if not is_single_character(get_arguments()) :
+        return
+    get_pyramid(get_arguments())
 
 # Partie 4 : Affichage
-display()
+display_pyramid()
 """
+Afficher un escalier constitué d’un caractère et d’un nombre d’étages donné en paramètre.
+
+Exemples d’utilisation :
+$> ruby exo.rb O 5
+    O
+   OOO
+  OOOOO
+ OOOOOOO
+OOOOOOOOO
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 """
