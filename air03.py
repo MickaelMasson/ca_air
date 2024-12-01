dc8be8096504eb9942af541b736e365fabbb348d
MickaelMasson <masson.mickael@me.com>
2024-12-01 13:07:05 +0100

    Chercher l'intrus / modif suite à la correction de l'eau

--- air03.py
+++ air03.py
@@ -2,35 +2,43 @@
 import sys
 
 # Fonctions utilisées
-def get_single_arguments(arguments) :
+def get_single_arguments(arguments: list[str]) -> list[str] :
     single_arguments = []
     for argument in arguments :
-        if argument in single_arguments :
+        is_pair = False
+        for single_argument in single_arguments :
+            if single_argument == argument :
+                is_pair = True
+                break
+        if is_pair :
             single_arguments.remove(argument)
         else :
             single_arguments.append(argument)
+
     return single_arguments
 
 # Partie 1 : Gestion d'erreur
-def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 1 :
-        print("Error, vous devez saisir au moins 1 argument")
+def is_valid_arguments(arguments: list, number_of_argument: int) -> bool:
+    if len(arguments) < number_of_argument :
+        print("Error, le nombre d'arguments n'est pas valide")
         return False
     return True
 
 # Partie 2 : Parsing
-def get_arguments() :
+def get_arguments() -> list :
     arguments = sys.argv[1:]
     return arguments
 
 # Partie 3 : Résolution
-def display_single_argument() :
-    if not is_valid_number_of_arguments(get_arguments()) :
+def display_single_arguments() :
+    arguments = get_arguments()
+    min_number_of_argument_expected = 2
+    if not is_valid_arguments(arguments, min_number_of_argument_expected) :
         return
-    print(", ".join(get_single_arguments(get_arguments())))
+    print(", ".join(get_single_arguments(arguments)))
 
 # Partie 4 : Affichage
-display_single_argument()
+display_single_arguments()
 """
 Créez un programme qui retourne la valeur d’une liste qui n’a pas de paire.
 
