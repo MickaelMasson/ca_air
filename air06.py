b2c41ff927bd016dfc5c649ac70038dc984ae7bb
MickaelMasson <masson.mickael@me.com>
2024-12-01 16:13:54 +0100

    Contrôle de pass sanitaire / modif suite à la correction de l'eau

--- air06.py
+++ air06.py
@@ -2,41 +2,49 @@
 import sys
 
 # Fonctions utilisées
-def get_arguments_and_key_string(arguments) :
-    base_arguments = arguments[0:-1]
-    key_string = str(arguments[-1]).lower()
-    return base_arguments, key_string
-
-
-def is_in_string(arguments, key_string):
-    news_arguments = []
-    for i in arguments :
-        if not key_string in str(i).lower() :
-            news_arguments.append(i)
-    return news_arguments
+def remove_arguments_with_substring(arguments: list[str], substring: str) -> list[str] :
+    arguments_without_substring = []
+    for argument in arguments :
+        for i in range(len(argument) - len(substring) + 1) :
+            is_found = False
+            if argument[i:i + len(substring)] == substring :
+                is_found = not is_found
+                break
+        if not is_found :
+            arguments_without_substring.append(argument)               
+    return arguments_without_substring
 
 # Partie 1 : Gestion d'erreur
-def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 2 :
-        print("Error, vous devez saisir au moins 2 arguments")
+def is_valid_arguments(arguments: list, number_of_argument: int) -> bool :
+    if len(arguments) < number_of_argument :
+        print("Error, le nombre d'arguments n'est pas valide")
         return False
     return True
 
 # Partie 2 : Parsing
-def get_arguments() :
+def get_arguments() -> list :
     arguments = sys.argv[1:]
     return arguments
 
 # Partie 3 : Résolution
-def display() :
-    if not is_valid_number_of_arguments(get_arguments()) :
+def display_arguments_without_substring() :
+    arguments = get_arguments()
+    min_number_of_argument_expected = 2
+    if not is_valid_arguments(arguments, min_number_of_argument_expected) :
         return
-    print(", ".join(is_in_string(get_arguments())))
+
+    base_arguments = list(map(str.lower, arguments[0:-1]))
+    substring = str(arguments[-1]).lower()
+
+    arguments_without_substring = remove_arguments_with_substring(base_arguments, substring)
+    title_arguments = list(map(str.title, arguments_without_substring))
+    print(" ".join(title_arguments))
 
 # Partie 4 : Affichage
-display()
+display_arguments_without_substring()
 """
-Créez un programme qui supprime d’un tableau tous les éléments qui ne contiennent pas une autre chaîne de caractères.
+Créez un programme qui supprime d’un tableau tous les éléments qui ne contiennent 
+pas une autre chaîne de caractères.
 
 Utilisez une fonction de ce genre (selon votre langage) :
 ma_fonction(array_de_strings, string) {
