b1a7cc3ceae4287cad6e7c8d37328d087071e57d
MickaelMasson <masson.mickael@me.com>
2024-12-01 18:31:30 +0100

    Mélanger deux tableaux triées / modif suite à la correction de l'eau

--- air08.py
+++ air08.py
@@ -2,60 +2,80 @@
 import sys
 
 # Fonctions utilisées
-def get_two_list(arguments) :
-    separator = "fusion"
-    index_separator = arguments.index(separator)
+def get_two_list(arguments: list[str], separator:str) -> tuple[list[str], list[str]]:
+    for i in range(len(arguments)) :
+        if separator == arguments[i] :
+            index_separator = i
+            break
     first_list = arguments[:index_separator]
     second_list = arguments[index_separator + 1:]
     return first_list, second_list
 
-def get_sorted_list(first_list, second_list) :
-    new_list = first_list + second_list
-    numbers = list(map(int, new_list))
-    sorted_numbers = sorted(numbers)
-    sorted_str_list = list(map(str, sorted_numbers))
-    return sorted_str_list
+def add_sorted_numbers_in_sorted_numbers(first_numbers: list[int], second_numbers: list[int]) -> list[int]:
+    new_sorted_numbers = first_numbers
+    for new_number in second_numbers :
+        for i in range(len(first_numbers)) :
+            if new_number < first_numbers[i] :
+                new_sorted_numbers.insert(i, new_number)
+                break
+        else:
+            new_sorted_numbers.append(new_number)
+    return new_sorted_numbers
 
 # Partie 1 : Gestion d'erreur
-def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 3 :
-        print("Error, vous devez saisir au moins 3 arguments")
+def is_valid_arguments(arguments: list, number_of_argument: int) -> bool:
+    if len(arguments) < number_of_argument :
+        print("Error, le nombre d'arguments n'est pas valide")
         return False
     return True
 
-def is_valid_separator(arguments) :
-    if not "fusion" in arguments :
-        print("Error, vous devez avoir un argument nommé 'fusion'")
-        return False
-    return True
-
-def is_digit(arguments) :
+def is_valid_separator(arguments: list[str], separator: str) -> bool:
     for argument in arguments :
-        if not argument.lstrip("-").isdigit() and argument != "fusion":
-            print("Error, hormis 'fusion' les arguments doivent être des entiers")
+            if argument == separator :
+                return True
+    print(f"Error, vous devez saisir '{separator}' en argument pour séparer vos deux listes")
+    return False
+
+def is_digit(string: str) -> bool:
+    string = string.lstrip("-")
+    for character in string :
+        if not "0" <= character <= "9" :
+            print(f"Error, '{string}' n'est pas un nombre entier positif")
             return False
     return True
 
 # Partie 2 : Parsing
-def get_arguments() :
+def get_arguments() -> list :
     arguments = sys.argv[1:]
     return arguments
 
 # Partie 3 : Résolution
-def display_sort_numbers() :
-    if not is_valid_number_of_arguments(get_arguments()) :
+def display_new_sorted_numbers() :
+    arguments = get_arguments()
+    min_number_of_argument_expected = 3
+    if not is_valid_arguments(arguments, min_number_of_argument_expected) :
         return
-    if not is_valid_separator(get_arguments()) :
+    separator = "fusion"
+    if not is_valid_separator(arguments, separator) :
         return
-    if not is_digit(get_arguments()) :
-        return
-    first_list, second_list = get_two_list(get_arguments())
-    print(" ".join(get_sorted_list(first_list, second_list)))
+    first_list, second_list = get_two_list(arguments, separator)
+    for argument in first_list :
+        if not is_digit(argument) :
+            return
+    first_numbers = list(map(int, first_list))
+    for argument in second_list :
+        if not is_digit(argument) :
+            return
+    second_numbers = list(map(int, second_list))
+    new_sorted_numbers = add_sorted_numbers_in_sorted_numbers(first_numbers, second_numbers)
+    new_sorted_list = list(map(str, new_sorted_numbers))
+    print(" ".join(new_sorted_list))
 
 # Partie 4 : Affichage
-display_sort_numbers()
+display_new_sorted_numbers()
 """
-Créez un programme qui fusionne deux listes d’entiers triées en les gardant triées, les deux listes seront séparées par un “fusion”.
+Créez un programme qui fusionne deux listes d’entiers triées en les gardant triées, 
+les deux listes seront séparées par un “fusion”.
 
 Utilisez une fonction de ce genre (selon votre langage) :
 sorted_fusion(array1, array2) {
