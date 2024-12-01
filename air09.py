b1c1fcf6ef0134b54649233ba790478e905460d2
MickaelMasson <masson.mickael@me.com>
2024-12-01 19:25:44 +0100

    Rotation vers la gauche / modif suite à la correction de l'eau

--- air09.py
+++ air09.py
@@ -2,33 +2,35 @@
 import sys
 
 # Fonctions utilisées
-def shifts_to_the_left(arguments) :
+def shifts_to_the_left(arguments: list) -> list:
     first_argument = arguments[0]
     arguments.remove(first_argument)
-    new_list = arguments
-    new_list.append(first_argument)
-    return new_list
+    new_sort_arguments = arguments
+    new_sort_arguments.append(first_argument)
+    return new_sort_arguments
 
 # Partie 1 : Gestion d'erreur
-def is_valid_number_of_arguments(arguments) :
-    if len(arguments) < 2 :
-        print("Error, vous deve saisir au moins deux arguments")
+def is_valid_arguments(arguments: list, number_of_argument: int) -> bool:
+    if len(arguments) < number_of_argument :
+        print("Error, le nombrre d'arguments n'est pas valide")
         return False
     return True
 
 # Partie 2 : Parsing
-def get_arguments() :
+def get_arguments() -> list :
     arguments = sys.argv[1:]
     return arguments
 
 # Partie 3 : Résolution
-def display_new_list() :
-    if not is_valid_number_of_arguments(get_arguments()) :
+def display_new_sort_arguments() :
+    arguments = get_arguments()
+    min_number_of_argument_expected = 2
+    if not is_valid_arguments(arguments, min_number_of_argument_expected) :
         return
-    print(", ".join(shifts_to_the_left(get_arguments())))
+    print(", ".join(shifts_to_the_left(arguments)))
 
 # Partie 4 : Affichage
-display_new_list()
+display_new_sort_arguments()
 """
 Créez un programme qui décale tous les éléments d’un tableau vers la gauche. Le premier élément devient le dernier à chaque rotation.
 
